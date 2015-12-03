531b14fe14c1986bbed7c2fed441c03208e18f4d
guneetsinghmehta <mehta_guneet@iitj.ac.in>
2015-12-02 23:38:02 -0600

    debug

--- S2v32.java
+++ S2v32.java
@@ -54,7 +54,7 @@ public class S2v32 {
 			//requestString=requestString.substring(0, requestString.length());
 			query=Integer.parseInt(requestString);
 			
-			if(query>=Datav2.NUM_UNIQUE_CHARACTERS)
+			if(query>Datav2.NUM_UNIQUE_CHARACTERS)
 			{
 				skt.close();
 				writer.close();
